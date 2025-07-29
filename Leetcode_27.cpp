16d849e7cae755822f461204385ec7a81908ba2c
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-07-29 10:56:40 +0530

    Update Leetcode_27.cpp

--- Leetcode_27.cpp
+++ Leetcode_27.cpp
@@ -1,10 +1,4 @@
 // 27. Remove Element
-// Solved
-// Easy
-// Topics
-// premium lock icon
-// Companies
-// Hint
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 // Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
