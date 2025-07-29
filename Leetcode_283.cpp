32d483b173bddd3e548d5ee1279796ef894f45c9
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-07-29 09:53:45 +0530

    Create Leetcode_283.cpp

---
+++ Leetcode_283.cpp
@@ -0,0 +1,41 @@
+// 283. Move Zeroes
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k=0,count=0;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                k++;
+            }
+            if(nums[i] == 0){
+                count++;
+            }
+        }
+
+        for(int i=k;i<n;i++){
+            nums[i] = 0;
+        }
+    }
+};
