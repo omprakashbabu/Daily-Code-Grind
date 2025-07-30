74d1409b8d11abf3882574cd12a8262f4628ba4f
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-07-31 00:00:31 +0530

    Create Leetcode_242.cpp

---
+++ Leetcode_242.cpp
@@ -0,0 +1,24 @@
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() < t.length()){
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
+};
