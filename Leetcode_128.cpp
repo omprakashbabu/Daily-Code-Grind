78ad3eec67f5dead414ab16e2b142a896816d6ea
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-01 23:55:12 +0530

    Update Leetcode_128.cpp

--- Leetcode_128.cpp
+++ Leetcode_128.cpp
@@ -14,3 +14,24 @@
 // Example 3:
 // Input: nums = [1,0,1,2]
 // Output: 3
+
+
+class Solution {
+ public:
+  int longestConsecutive(vector<int>& nums) {
+    int ans = 0;
+    unordered_set<int> seen{nums.begin(), nums.end()};
+
+    for (int num : seen) {
+      // `num` is the start of a sequence.
+      if (seen.contains(num - 1))
+        continue;
+      int length = 1;
+      while (seen.contains(++num))
+        ++length;
+      ans = max(ans, length);
+    }
+
+    return ans;
+  }
+};
