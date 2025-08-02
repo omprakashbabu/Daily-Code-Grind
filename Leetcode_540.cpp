7b871a38044319820d5a2e5652c6f7130064915b
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-02 14:19:11 +0530

    Update Leetcode_540.cpp

--- Leetcode_540.cpp
+++ Leetcode_540.cpp
@@ -11,3 +11,25 @@
 // Input: nums = [3,3,7,7,10,11,11]
 // Output: 10
 
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int start = 0, end = nums.size() - 1;
+        
+        while (start < end) {
+            int mid = (start + end) / 2;
+            
+            // Ensure mid is even for comparison with mid+1
+            if (mid % 2 == 1) mid--;
+
+            // If pair is intact on the left, move right
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return nums[start];
+    }
+};
