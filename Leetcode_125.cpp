8abfc2021e9b36e398a1d431c5d10b4279b5428b
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-07 23:46:42 +0530

    Update Leetcode_125.cpp

--- Leetcode_125.cpp
+++ Leetcode_125.cpp
@@ -17,3 +17,24 @@
 // Output: true
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length() <= 1)return true;
+        int start = 0, end = s.length()-1;
+
+        while(start < end){
+            while(start < end && !isalnum(s[start])) start++;
+            while(start < end && !isalnum(s[end])) end--;
+
+            if(start < end && tolower(s[start]) != tolower(s[end])) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
+
+
