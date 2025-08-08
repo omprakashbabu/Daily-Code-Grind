cc200187b4c74318a1a11b6f95c55727bd913fc2
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-08 23:02:26 +0530

    Update Leetcode_167.cpp

--- Leetcode_167.cpp
+++ Leetcode_167.cpp
@@ -13,7 +13,7 @@
 // Example 2: 
 // Input: numbers = [2,3,4], target = 6
 // Output: [1,3]
-// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3]. 
 
 // Example 3:
 // Input: numbers = [-1,0], target = -1
