7063028452f20eb20ec866fd462d68c7fb50e460
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-12 10:00:15 +0530

    Update Leetcode_14.cpp

--- Leetcode_14.cpp
+++ Leetcode_14.cpp
@@ -11,3 +11,21 @@
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
  
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty() || strs.size() == 0) return "";
+
+        string prefix = strs[0];
+
+        for(int i=1;i<strs.size();i++){
+            while(strs[i].find(prefix) != 0){
+                prefix = prefix.substr(0,prefix.length()-1);
+
+                if(prefix.length() == 0) return "";
+            }
+        }
+        return prefix;
+    }
+};
+
