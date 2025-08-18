9803edb5914cee0d3c540158cf7426a9785ce73c
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-08-18 17:32:21 +0530

    Update Leetocde_35.cpp

--- Leetocde_35.cpp
+++ Leetocde_35.cpp
@@ -13,3 +13,28 @@
 // Example 3:
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0, end = n-1;
+
+        while(start <= end){
+            int mid = (start+end) / 2;
+
+            if(target == nums[mid]) {
+                return mid;
+            }
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else if(nums[mid] < target){
+                start = mid+1;
+            }
+
+        }
+        return start;
+    }
+};
