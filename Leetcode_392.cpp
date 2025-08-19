c951ef63f61162ac26f7f430e03831876ca6ca7a
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-08-19 21:50:05 +0530

    Update Leetcode_392.cpp

--- Leetcode_392.cpp
+++ Leetcode_392.cpp
@@ -9,3 +9,19 @@
 // Example 2:
 // Input: s = "axc", t = "ahbgdc"
 // Output: false
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0, j=0;
+
+        while(i < s.size() && j < t.size()){
+            if(s[i] == t[j]){
+                i++;            
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
+
