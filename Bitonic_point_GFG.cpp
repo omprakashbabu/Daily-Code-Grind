aac8adf9455c357c882ded4148c3d90ff28e90af
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-08-21 23:06:50 +0530

    Update Bitonic_point_GFG.cpp

--- Bitonic_point_GFG.cpp
+++ Bitonic_point_GFG.cpp
@@ -12,7 +12,7 @@
 // Explanation: Elements before 8 are strictly increasing [1, 2, 4, 5, 7] and elements after 8 are strictly decreasing [3].
 // Input: arr[] = [10, 20, 30, 40, 50]
 // Output: 50
-// Explanation: Elements before 50 are strictly increasing [10, 20, 30 40] and there are no elements after 50.
+// Explanation: Elements before 50 are strictly increasing [10, 20, 30 40] and there are no elements after 50. 
 // Input: arr[] = [120, 100, 80, 20, 0] 
 // Output: 120
 // Explanation: There are no elements before 120 and elements after 120 are strictly decreasing [100, 80, 20, 0].
