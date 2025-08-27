4d127191529ab85856791a93bf307304fd695a9d
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-08-27 23:21:41 +0530

    Update Leetcode_136.cpp

--- Leetcode_136.cpp
+++ Leetcode_136.cpp
@@ -18,3 +18,15 @@ Constraints:
 1 <= nums.length <= 3 * 104
 -3 * 104 <= nums[i] <= 3 * 104
 Each element in the array appears twice except for one element which appears only once.
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(auto i : nums){
+            result ^= i;
+        }
+        return result;
+    }
+};
