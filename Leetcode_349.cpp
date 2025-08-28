8bfed8f4468f24f56adee6c363f0869868f4fb51
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-08-28 23:53:13 +0530

    Update Leetcode_349.cpp

--- Leetcode_349.cpp
+++ Leetcode_349.cpp
@@ -1,9 +1,4 @@
 349. Intersection of Two Arrays
-Solved
-Easy
-Topics
-premium lock icon
-Companies
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
  
