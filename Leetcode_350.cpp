2f187eb633827eb8b344e1ba3bbfb771488b22c4
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-02 20:41:16 +0530

    Update Leetcode_350.cpp

--- Leetcode_350.cpp
+++ Leetcode_350.cpp
@@ -9,3 +9,28 @@
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [4,9]
 // Explanation: [9,4] is also accepted.
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;  // map to store count of elements from nums1
+        vector<int> ans;
+
+        // Count each number in nums1
+        for (auto x : nums1) {
+            mp[x]++;
+        }
+
+        // Check elements in nums2
+        for (auto x : nums2) {
+            if (mp[x] > 0) {
+                ans.push_back(x); // element is in both arrays
+                mp[x]--;          // decrease the count to handle duplicates properly
+            }
+            else continue; // if not found or used up, skip
+        }
+
+        return ans;
+      
+    }
+};
