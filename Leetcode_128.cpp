e49ddef4b9254bbceada5ae8541926bae0241c48
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-10 23:49:34 +0530

    Update Leetcode_128.cpp

--- Leetcode_128.cpp
+++ Leetcode_128.cpp
@@ -1,6 +1,6 @@
 // 128. Longest Consecutive Sequence
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-// You must write an algorithm that runs in O(n) time.
+// You must write an algorithm that runs in O(n) time. 
 
 // Example 1: 
 // Input: nums = [100,4,200,1,3,2]
