847c10ed64a39c25997f55a5f2a736cfb3911e02
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-12 23:52:29 +0530

    Update Extra_element_GFG.cpp

--- Extra_element_GFG.cpp
+++ Extra_element_GFG.cpp
@@ -3,7 +3,7 @@ You have given two sorted arrays arr1[] & arr2[] of distinct elements. The first
 Note: 0-based indexing is followed.
 
 Examples 
-Input: a[] = [2,4,6,8,9,10,12], b[] = [2,4,6,8,10,12]
+Input: a[] = [2,4,6,8,9,10,12], b[] = [2,4,6,8,10,12] 
 Output: 4
 Explanation: In the first array, 9 is extra added and it's index is 4.
 Input: a[] = [3,5,7,8,11,13], b[] = [3,5,7,11,13] 
