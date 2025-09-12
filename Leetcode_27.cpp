4b7a6a762838546e9377f23c85980b89e905d1d4
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-13 01:02:10 +0530

    Update Leetcode_27.cpp

--- Leetcode_27.cpp
+++ Leetcode_27.cpp
@@ -71,3 +71,15 @@ public:
         return nums.size();
     }
 };
+
+//Solution 3 - Python
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
+
