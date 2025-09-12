df0f2ac263c51b443c8096d8f1d5b8e5982fb77a
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-12 23:52:20 +0530

    Update Extra_element_GFG.cpp

--- Extra_element_GFG.cpp
+++ Extra_element_GFG.cpp
@@ -2,7 +2,7 @@
 You have given two sorted arrays arr1[] & arr2[] of distinct elements. The first array has one element extra added in between. Return the index of the extra element.
 Note: 0-based indexing is followed.
 
-Examples
+Examples 
 Input: a[] = [2,4,6,8,9,10,12], b[] = [2,4,6,8,10,12]
 Output: 4
 Explanation: In the first array, 9 is extra added and it's index is 4.
