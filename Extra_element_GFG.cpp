50ca0bf7cf5ce8f7a5606bdebf3bc9bbfd59f2e7
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-15 23:45:11 +0530

    Update Extra_element_GFG.cpp

--- Extra_element_GFG.cpp
+++ Extra_element_GFG.cpp
@@ -6,7 +6,7 @@ Examples
 Input: a[] = [2,4,6,8,9,10,12], b[] = [2,4,6,8,10,12] 
 Output: 4
 Explanation: In the first array, 9 is extra added and it's index is 4.
-Input: a[] = [3,5,7,8,11,13], b[] = [3,5,7,11,13] 
+Input: a[] = [3,5,7,8,11,13], b[] = [3,5,7,11,13]  
 Output: 3 
 Explanation: In the first array, 8 is extra and it's index is 3.
 Input: a[] = [3,5], b[] = [3]
