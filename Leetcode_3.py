8a253c378f34109baabb8bd6603ba3c4bb6af7b1
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-16 00:57:48 +0530

    Update Leetcode_3.py

--- Leetcode_3.py
+++ Leetcode_3.py
@@ -17,4 +17,15 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res_set = set()
+        l=0
+        res = 0
+        for i in range(0,len(s)):
+            while s[i] in res_set:
+                res_set.remove(s[l])
+                l += 1
+            res_set.add(s[i])
+            res = max(res, i-l+1)
+        return res
