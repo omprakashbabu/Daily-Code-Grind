e7c26ebba0de43c695c58fa1965a83b0872cfbac
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-18 23:33:29 +0530

    Update Bitonic_point_GFG.cpp

--- Bitonic_point_GFG.cpp
+++ Bitonic_point_GFG.cpp
@@ -23,7 +23,7 @@ class Solution {
     int findMaximum(vector<int> &arr) {
         // code here 
         int start = 0;
-        int end = arr.size()-1;
+        int end = arr.size()-1; 
         
         while(start <= end){
             int mid = (start + end)/2;
