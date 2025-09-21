f29955abab54c4edb1732cfd8946d4f9d07a61a2
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-21 23:54:25 +0530

    Update Leetcode_14.cpp

--- Leetcode_14.cpp
+++ Leetcode_14.cpp
@@ -29,3 +29,19 @@ public:
     }
 };
 
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_len = float('inf')
+        for s in strs:
+            if len(s) < min_len:
+                min_len = len(s)
+
+        i = 0
+        while i < min_len:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+
+        return s[:i]
+
