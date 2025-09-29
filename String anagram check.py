ae2a881ab4601237c152259e6c1383ec87dd6da7
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-29 23:36:02 +0530

    Update String anagram check.py

--- String anagram check.py
+++ String anagram check.py
@@ -1,3 +1,9 @@
+Problem: Check If Two Strings Are Anagrams
+Description:
+Given two strings str1 and str2, determine if they are anagrams of each other.
+Two strings are anagrams if they contain the same characters with the same frequencies, but possibly in a different order.
+
+
 def anagrams(str1, str2):
     if len(str1) != len(str2):
         return False
